f741e7d82e8639b3b05d46633e9a06315d89960c
Dhruv Chandra <87961794+Dhruv-Chandra@users.noreply.github.com>
2024-12-26 17:50:54 +0000

    bug fix

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from openai import OpenAI, OpenAIError
 st.set_page_config(page_title="BlogBotter 💬 ", initial_sidebar_state="auto")
 st.title("BlogBotter 💬 ")
 
-# nltk.download("stopwords")
-# nltk.download("wordnet")
+nltk.download("stopwords")
+nltk.download("wordnet")
 warnings.filterwarnings("ignore")
 
 if "user" not in st.session_state:
